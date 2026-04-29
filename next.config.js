7630548ca2187e8a74235d8c1070a87b2fcfc072
phamngocduongduong-ui <phamngocduong.duong@gmail.com>
2026-04-30 02:01:17 +0700

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: "export",
+    basePath: "/Sapo-Github",
+    images: {
+          unoptimized: true,
+    },
+};
+
+module.exports = nextConfig;
